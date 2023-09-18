c8635498378a75088c43464b75314d5e0ed48446
Tien-Anh-1123 <112912496+Tien-Anh-1123@users.noreply.github.com>
2023-09-18 20:04:05 +0700

    Update vnpay_return.php

--- vnpay_return.php
+++ vnpay_return.php
@@ -44,7 +44,7 @@
     $vnp_TmnCode = "CGXZLS0Z"; //Mã định danh merchant kết nối (Terminal Id)
     $vnp_HashSecret = "XNBCJFAKAZQSGTARRLGCHVZWCIOIGSHN"; //Secret key
     $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-    $vnp_Returnurl = "https://munonchat.azurewebsites.net/vnpay_php/vnpay_return.php";
+    $vnp_Returnurl = "https://munonchat.azurewebsites.net/vnpay_return.php";
     $vnp_apiUrl = "http://sandbox.vnpayment.vn/merchant_webapi/merchant.html";
     $apiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
     //Config input format
