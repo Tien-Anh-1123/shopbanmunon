5160a11871b5c201c432c7399e412e1cd5d88f74
Tien-Anh-1123 <112912496+Tien-Anh-1123@users.noreply.github.com>
2023-09-18 20:46:19 +0700

    Update connect.php

--- admin/connect.php
+++ admin/connect.php
@@ -1,4 +1,9 @@
 <?php
-$connect = mysqli_init();
-mysqli_ssl_set($connect, NULL, NULL, "./DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($connect, "munonchat.mysql.database.azure.com", "tienanhdz", "phuc17@@", "ban_mu", 3306, MYSQLI_CLIENT_SSL);
+
+$connect = mysqli_connect('database-1.cnfp5xsngx3p.ap-southeast-2.rds.amazonaws.com', 'admin', 'Huyphan2002', 'ban_mu');
+
+mysqli_set_charset($connect, 'utf8');
+
+// $connect = mysqli_init();
+// mysqli_ssl_set($connect, NULL, NULL, "./DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+// mysqli_real_connect($connect, "munonchat.mysql.database.azure.com", "tienanhdz", "phuc17@@", "ban_mu", 3306, MYSQLI_CLIENT_SSL);
